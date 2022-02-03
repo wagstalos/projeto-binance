c682c0c4d818a73f208cb3be6c61b4bbd1949065
Wagner Paulo <wagstalos@hotmail.com>
2022-02-02 21:43:25 -0300

    add tiped

--- js/main.js
+++ js/main.js
@@ -4,3 +4,20 @@ const menuMobile = document.getElementById("menu-mobile");
 botoes.addEventListener("click", () => {
   menuMobile.classList.toggle("active");
 });
+
+// var typed = new Typed(".element", {
+//   strings: ["Aprenda programação voltada para blockchain"],
+//   typeSpeed: 60,
+//   loop: true,
+// });
+var typed3 = new Typed(".element", {
+  strings: [
+    "Aprenda programação voltada: criptomoedas",
+    "Aprenda programação voltada: NFT",
+    "Aprenda programação voltada: Blockchain",
+  ],
+  typeSpeed: 50,
+  backSpeed: 0,
+  smartBackspace: true,
+  // this is a default
+});
--- js/particules.js
+++ js/particules.js
@@ -9,7 +9,6 @@ particlesJS("particles-js", {
       type: "triangle",
       stroke: { width: 0, color: "#000000" },
       polygon: { nb_sides: 5 },
-      image: { src: "img/github.svg", width: 100, height: 100 },
     },
     opacity: {
       value: 0.2243374034547962,
@@ -56,20 +55,3 @@ particlesJS("particles-js", {
   },
   retina_detect: true,
 });
-var count_particles, stats, update;
-stats = new Stats();
-stats.setMode(0);
-stats.domElement.style.position = "absolute";
-stats.domElement.style.left = "0px";
-stats.domElement.style.top = "0px";
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector(".js-count-particles");
-update = function () {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-  }
-  requestAnimationFrame(update);
-};
-requestAnimationFrame(update);
